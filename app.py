8a23616c141a02244927ba2408bf6b253d090e66
smalsmalhero <152018837+smalsmalhero@users.noreply.github.com>
2023-11-26 11:08:05 +0800

    Update 1app.py

--- app.py
+++ app.py
@@ -71,5 +71,7 @@ def home():
   except:
     print('error')
 
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
